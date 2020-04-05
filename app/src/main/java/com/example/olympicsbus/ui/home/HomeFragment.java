bc22c8d8bf88bd5e17b7991b1161644d07757c89
mprngpkt <mp.mppkt@gmail.com>
2020-04-05 22:16:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/olympicsbus/ui/home/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.olympicsbus.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.olympicsbus.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button bus1 = (Button)view.findViewById(R.id.event1Button);
+        bus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.home_to_bus1);
+            }
+        });
+    }
+}
